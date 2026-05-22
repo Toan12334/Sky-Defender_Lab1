cf826285db815a029d20d2f932f4374a96208d69
dao xuan toan <daoxuant2@gmail.com>
2026-05-22 18:36:03 +0700

    done scene

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    private SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        // Tự động tìm hoặc thêm SceneLoader vào cùng một Object để sử dụng
+        sceneLoader = GetComponent<SceneLoader>();
+        if (sceneLoader == null) sceneLoader = gameObject.AddComponent<SceneLoader>();
+    }
+
+    // Gán hàm này vào Event OnClick của nút Start
+    public void OnClickStart()
+    {
+        sceneLoader.LoadSceneByName("Gameplay");
+    }
+
+    // Gán hàm này vào nút Restart
+    public void OnClickRestart()
+    {
+        sceneLoader.LoadSceneByName("Gameplay");
+    }
+
+    // Gán hàm này vào nút MainMenu
+    public void OnClickMainMenu()
+    {
+        sceneLoader.LoadSceneByName("MainMenu");
+    }
+
+    // Gán hàm này vào nút Quit
+    public void OnClickQuit()
+    {
+        sceneLoader.QuitGame();
+    }
+}
